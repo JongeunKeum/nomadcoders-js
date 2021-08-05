cf6956f47e421243b01a263683552db3e2bb0841
JongeunKeum <jon9eun@gmail.com>
2021-08-05 11:58:56 +0900

    lecture #8.1 weather

--- weather/js/weather.js
+++ weather/js/weather.js
@@ -1,9 +1,17 @@
-/********** #8.0 **********/
+/********** #8.1 **********/
+
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";	// not mine
 
 function onGeoSuccess(position) {
 	const lat = position.coords.latitude;
 	const lon = position.coords.longitude;
-	console.log("You live in", lat, lng);
+	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+	fetch(url).then(response => response.json()).then(data => {
+		city.innerText = date.name;
+		weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+	});
 }
 
 function onGeoError() {
